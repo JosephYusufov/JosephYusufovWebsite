c783394747e6af091220c732fa495f4b151a4413
JosephYusufov <jyusufov00@stuy.edu>
2019-08-22 19:33:54 -0400

    changed port to 8080

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.set('view engine', 'ejs')
 app.use('/static', express.static('static'))
 app.use('/js', express.static('js'))
 
-app.listen('3000', () => {
-    console.log('Server started on port 3000')
+app.listen('8080', () => {
+    console.log('Server started on port 8080')
 });
 
 app.get('/', (req, res)=> {
